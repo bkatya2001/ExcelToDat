8c3def4fdcd696e61c290b53fc792b2b9f1dc5bf
Ekaterina Bolshakova <ekaterinabolshakova2001@yandex.ru>
2021-04-10 15:17:46 +0300

    Base version

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-### Convertor tables to .dat
+### Convertor of tables to .dat
 
 Данное десктопное приложение позволяет выбрать excel-файл с компьютера 
 и преобразовать его в .dat-файл.
--- mywindow.py
+++ mywindow.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtWidgets import QVBoxLayout, QPushButton
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QFileDialog
-from PyQt5.QtCore import QSize, Qt, pyqtSlot
+from PyQt5.QtCore import QSize, pyqtSlot
+import pandas as pd
 import sys
 
 
@@ -13,55 +14,48 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
 
         self.setMinimumSize(QSize(480, 80))
-        self.setWindowTitle("Работа с QTableWidget")
-        central_widget = QWidget(self)  # Создаём центральный виджет
-        self.setCentralWidget(central_widget)
+        self.setWindowTitle("Convertor from .xlsx to .dat")
+        self.central_widget = QWidget(self)  # Создаём центральный виджет
+        self.setCentralWidget(self.central_widget)
 
-        vertical_layout = QVBoxLayout()
-        central_widget.setLayout(vertical_layout)
+        self.vertical_layout = QVBoxLayout() # Вертикальная расстановка
+        self.central_widget.setLayout(self.vertical_layout)
+        
+        self.table = QTableWidget(self) # Пустая таблица
 
-        get_btn = QPushButton("Choose file", self)
-        get_btn.clicked.connect(self.choose_file)
-        convert_btn = QPushButton("Get .dat", self)
-        convert_btn.clicked.connect(self.convert_data)
-
-        table = QTableWidget(self)  # Создаём таблицу
-        table.setColumnCount(3)     # Устанавливаем три колонки
-        table.setRowCount(1)        # и одну строку в таблице
-
-        # Устанавливаем заголовки таблицы
-        table.setHorizontalHeaderLabels(["Header 1", "Header 2", "Header 3"])
-
-        # Устанавливаем всплывающие подсказки на заголовки
-        table.horizontalHeaderItem(0).setToolTip("Column 1 ")
-        table.horizontalHeaderItem(1).setToolTip("Column 2 ")
-        table.horizontalHeaderItem(2).setToolTip("Column 3 ")
-
-        # Устанавливаем выравнивание на заголовки
-        table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
-        table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
-        table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
-
-        # заполняем первую строку
-        table.setItem(0, 0, QTableWidgetItem("Text in column 1"))
-        table.setItem(0, 1, QTableWidgetItem("Text in column 2"))
-        table.setItem(0, 2, QTableWidgetItem("Text in column 3"))
-
-        # делаем ресайз колонок по содержимому
-        table.resizeColumnsToContents()
-
-        vertical_layout.addWidget(get_btn)
-        vertical_layout.addStretch(1)
-        vertical_layout.addWidget(table)   # Добавляем таблицу в сетку
-        vertical_layout.addStretch(1)
-        vertical_layout.addWidget(convert_btn)
+        # Кнопки
+        self.get_btn = QPushButton("Choose file", self)
+        self.get_btn.clicked.connect(self.choose_file)
+        self.convert_btn = QPushButton("Get .dat", self)
+        self.convert_btn.clicked.connect(self.convert_data)
+        
+        # Добавление компонентов в расстановку
+        self.vertical_layout.addWidget(self.get_btn)
+        self.vertical_layout.addStretch(1)
+        self.vertical_layout.addWidget(self.table)
+        self.vertical_layout.addStretch(1)
+        self.vertical_layout.addWidget(self.convert_btn)
 
     @pyqtSlot()
     def choose_file(self):
-        print('PyQt5 button click')
+        filename = QFileDialog.getOpenFileName(self, "Выбрать таблицу",
+                                               ".", "Excel Workbook (*.xlsx)")
+        self.data = pd.read_excel(filename[0])
+        headers = self.data.columns.to_list()
+        
+        self.table.setColumnCount(len(headers))
+        self.table.setRowCount(len(self.data))
+        self.table.setHorizontalHeaderLabels(headers)
+        
+        for i in range(len(headers)):
+            for j in range(len(self.data)):
+                self.table.setItem(j, i, QTableWidgetItem(str(self.data.iloc[j, i])))
+                
+        # делаем ресайз колонок по содержимому
+        self.table.resizeColumnsToContents()
 
     def convert_data(self):
-        print('PyQt5 button click')
+        self.data.to_csv('out.dat', header=False, index=False)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
