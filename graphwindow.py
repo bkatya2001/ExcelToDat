5f2cd59fd71ede289682388e7391b35fc17f0eec
Ekaterina Bolshakova <ekaterinabolshakova2001@yandex.ru>
2021-04-18 19:03:03 +0300

    Graph was added

---
+++ graphwindow.py
@@ -0,0 +1,36 @@
+import random
+
+from PyQt5.QtWidgets import QMainWindow
+import pyqtgraph as pg
+import mywindow as mw
+
+
+def get_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+class GraphWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.graphWidget = pg.PlotWidget()
+        self.graphWidget.setBackground('w')
+        self.graphWidget.setTitle("Graph")
+        self.graphWidget.setLabel('left', 'Values')
+        self.graphWidget.setLabel('bottom', mw.x)
+        self.graphWidget.showGrid(x=True, y=True)
+        self.graphWidget.addLegend()
+        self.setCentralWidget(self.graphWidget)
+
+        # plot data: x, y values
+        for col in mw.data.columns:
+            if col != mw.data.columns[0]:
+                self.plot(mw.x, list(map(lambda d: float(d), mw.data[col])), col)
+
+    def plot(self, x, y, plotname):
+        color = get_color()
+        pen = pg.mkPen(color=(color[0], color[1], color[2]))
+        self.graphWidget.plot(x, y, name=plotname, pen=pen)
--- mywindow.py
+++ mywindow.py
@@ -2,9 +2,11 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QMessageBox
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QFileDialog
 from PyQt5.QtCore import QSize, pyqtSlot
+import graphwindow as gw
 import pandas as pd
 
 data = pd.DataFrame()
+x = []
 
 # Наследуемся от QMainWindow
 class MainWindow(QMainWindow):
@@ -18,12 +20,12 @@ class MainWindow(QMainWindow):
         self.central_widget = QWidget(self)  # Создаём центральный виджет
         self.setCentralWidget(self.central_widget)
 
-        self.vertical_layout = QVBoxLayout() # Вертикальная расстановка
+        self.vertical_layout = QVBoxLayout()  # Вертикальная расстановка
         self.central_widget.setLayout(self.vertical_layout)
-        
-        self.horizontal_layout = QHBoxLayout() # Горизонтальная расстановка
-        
-        self.table = QTableWidget(self) # Пустая таблица
+
+        self.horizontal_layout = QHBoxLayout()  # Горизонтальная расстановка
+
+        self.table = QTableWidget(self)  # Пустая таблица
 
         # Кнопки
         self.get_btn = QPushButton("Choose file", self)
@@ -32,34 +34,35 @@ class MainWindow(QMainWindow):
         self.convert_btn.clicked.connect(self.convert_data)
         self.graph_btn = QPushButton("Get graph", self)
         self.graph_btn.clicked.connect(self.get_graph)
-        
+
         # Добавление компонентов в расстановку
         self.vertical_layout.addWidget(self.get_btn)
         self.vertical_layout.addWidget(self.table)
         self.horizontal_layout.addWidget(self.convert_btn)
         self.horizontal_layout.addWidget(self.graph_btn)
         self.vertical_layout.addLayout(self.horizontal_layout)
-        
+
         # Возможность редактирования данных
         self.table.cellChanged.connect(self.change_cell)
 
     @pyqtSlot()
     def choose_file(self):
         global data
-        
+        global x
         filename = QFileDialog.getOpenFileName(self, "Выбрать таблицу",
                                                ".", "Excel Workbook (*.xlsx)")
         data = pd.read_excel(filename[0])
+        x = list(map(lambda d: float(d), data['A']))
         headers = data.columns.to_list()
-        
+
         self.table.setColumnCount(len(headers))
         self.table.setRowCount(len(data))
         self.table.setHorizontalHeaderLabels(headers)
-        
+
         for i in range(len(headers)):
             for j in range(len(data)):
                 self.table.setItem(j, i, QTableWidgetItem(str(data.iloc[j, i])))
-                
+
         # делаем ресайз колонок по содержимому
         self.table.resizeColumnsToContents()
 
@@ -67,11 +70,11 @@ class MainWindow(QMainWindow):
         global data
         data.to_csv('out.data', header=False, index=False)
         QMessageBox.about(self, "Conversion", "Conversion completed")
-        
+
     def get_graph(self):
         global data
-        print(data)
-        
+        self.graph_win = gw.GraphWindow()
+        self.graph_win.show()
+
     def change_cell(self, row, column):
         data.loc[row, data.columns[column]] = self.table.item(row, column).text()
-        
