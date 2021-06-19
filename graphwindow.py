62013567d5b17633332c643a384b0dd5c5917cd9
Ekaterina Bolshakova <ekaterinabolshakova2001@yandex.ru>
2021-06-19 15:03:47 +0300

    Window and original graph

--- graphwindow.py
+++ graphwindow.py
@@ -1,10 +1,10 @@
 import random
 
 import pandas as pd
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QHBoxLayout, QDesktopWidget, QApplication
 import pyqtgraph as pg
 import pyqtgraph.exporters
-import mywindow as mw
+import datawindow as dw
 import filterwindow as fw
 
 
@@ -19,23 +19,38 @@ class GraphWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        self.file_path = ""
         # Создаём поле для графика
-        self.graphWidget = pg.PlotWidget()
-        self.graphWidget.setBackground('w')
-        self.graphWidget.setTitle("Graph")
-        self.graphWidget.setLabel('left', 'Values')
-        self.graphWidget.setLabel('bottom', mw.x_name)
-        self.graphWidget.showGrid(x=True, y=True)
-        self.graphWidget.addLegend()  # Описание: цвет - график
+        self.originalGraphWidget = pg.PlotWidget()
+        self.originalGraphWidget.setBackground('w')
+        self.originalGraphWidget.setTitle("Original graph")
+        self.originalGraphWidget.setLabel('left', 'Values')
+        self.originalGraphWidget.setLabel('bottom', dw.x_name)
+        self.originalGraphWidget.showGrid(x=True, y=True)
+        self.originalGraphWidget.addLegend()  # Описание: цвет - график
+
+        self.changedGraphWidget = pg.PlotWidget()
+        self.changedGraphWidget.setBackground('w')
+        self.changedGraphWidget.setTitle("Changed graph")
+        self.changedGraphWidget.setLabel('left', 'Values')
+        self.changedGraphWidget.setLabel('bottom', dw.x_name)
+        self.changedGraphWidget.showGrid(x=True, y=True)
+        self.changedGraphWidget.addLegend()
 
         # Кнопки
         self.filter_btn = QPushButton("Filter", self)
         self.filter_btn.clicked.connect(self.filter_data)
+        self.addData_btn = QPushButton("Дополнить", self)
+        self.addData_btn.clicked.connect(self.add_data)
 
         self.vertical_layout = QVBoxLayout()
-        self.vertical_layout.addWidget(self.graphWidget)
-        self.vertical_layout.addWidget(self.filter_btn)
+        self.graph_layout = QHBoxLayout()
+        self.graph_layout.addWidget(self.originalGraphWidget)
+        self.graph_layout.addWidget(self.changedGraphWidget)
+        self.vertical_layout.addLayout(self.graph_layout)
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addWidget(self.filter_btn)
+        self.button_layout.addWidget(self.addData_btn)
+        self.vertical_layout.addLayout(self.button_layout)
 
         self.central_widget = QWidget(self)  # Создаём центральный виджет
         self.setCentralWidget(self.central_widget)
@@ -45,34 +60,47 @@ class GraphWindow(QMainWindow):
 
         self.save_graph("Graph")
 
+        #self.location_on_the_screen()
+
     def plot(self, x, y, plotname):
         color = get_color()
         pen = pg.mkPen(color=(color[0], color[1], color[2]))
-        return self.graphWidget.plot(x, y, name=plotname, pen=pen)
+        return self.originalGraphWidget.plot(x, y, name=plotname, pen=pen)
 
     def save_graph(self, filename):
-        exporter = pg.exporters.ImageExporter(self.graphWidget.plotItem)
-        exporter.params.param('width').setValue(self.graphWidget.range.width(), blockSignal=exporter.widthChanged)
-        exporter.params.param('height').setValue(self.graphWidget.range.height(), blockSignal=exporter.heightChanged)
+        exporter = pg.exporters.ImageExporter(self.originalGraphWidget.plotItem)
+        exporter.params.param('width').setValue(self.originalGraphWidget.range.width(), blockSignal=exporter.widthChanged)
+        exporter.params.param('height').setValue(self.originalGraphWidget.range.height(), blockSignal=exporter.heightChanged)
 
         # save to file
         exporter.export(filename + '.png')
 
     def draw_graph(self):
         # plot data: x, y values
-        for col in mw.y:
-            if col != mw.x_name:
+        for col in dw.y:
+            if col != dw.x_name:
                 flag = False
                 for fc in fw.y_filtered.keys():
                     if fc == col:
                         flag = True
                         break
                 if flag:
-                    plt = self.plot(mw.x, list(fw.y_filtered.get(col)), col)
+                    plt = self.plot(dw.x, list(fw.y_filtered.get(col)), col)
                 else:
-                    plt = self.plot(mw.x, list(mw.data[col]), col)
+                    plt = self.plot(dw.x, list(dw.data[col]), col)
+
+    def add_data(self):
+        print(0)
 
     def filter_data(self):
         self.filter_win = fw.FilterWindow()
         self.filter_win.show()
         self.close()
+
+    def location_on_the_screen(self):
+        screen_geometry = QApplication.desktop().availableGeometry()
+        screen_size = (screen_geometry.width(), screen_geometry.height())
+        win_size = (self.frameSize().width(), self.frameSize().height())
+        x = int(screen_size[0] / 2 - win_size[0] / 2)
+        y = int(screen_size[1] / 2 - win_size[1] / 2)
+        self.move(x, y)
