7f085934c16a0b33d1d693e553557abb545b4b07
Ekaterina Bolshakova <ekaterinabolshakova2001@yandex.ru>
2021-09-20 15:03:20 +0300

    QSlide bigger

--- start.py
+++ start.py
@@ -7,7 +7,11 @@ from qt_material import apply_stylesheet
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     apply_stylesheet(app, theme='dark_lightgreen.xml')
+    stylesheet = app.styleSheet()
+    with open('custom.css') as file:
+        app.setStyleSheet(stylesheet + file.read().format(**os.environ))
 
     try:
         os.mkdir(os.path.join(os.getcwd(), "Projects"))
